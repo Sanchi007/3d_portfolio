d792ec49f9f42b9eeac3f7268c4d2b0e8063d82b
Sanchi007 <me.sanchi007s@gmail.com>
2026-01-08 22:54:57 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+import Island from "../models/Island";
+{
+  /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+      Popup
+    </div> */
+}
+const Home = () => {
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 5, 10]} intensity={2} />
+          <spotLight
+            position={[0, 50, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
+          <hemisphereLight
+            skyColor='#b1e1ff'
+            groundColor='#000000'
+            intensity={1}
+          />
+          <Island />
+        </Suspense>
+
+
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
