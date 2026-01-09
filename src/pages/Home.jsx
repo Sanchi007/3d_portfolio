79b4c79615e62982b3d022030e107c2e859847e5
Sanchi007 <me.sanchi007s@gmail.com>
2026-01-09 08:29:19 +0530

    bird, plane and sky added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,27 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
+import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
       Popup
     </div> */
 }
 const Home = () => {
+  const adjustIslandForScreenSize =()=>{
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+    if(window.innerWidth < 768){
+      screenScale = [0.9, 0.9, 0.9];
+    } else{
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPosition, rotation, ];
+  }
+  const [islandScale, islandPosition,rotation] = adjustIslandForScreenSize();
   return (
     <section className="w-full h-screen relative">
       <Canvas
@@ -17,19 +32,15 @@ const Home = () => {
         <Suspense fallback={<Loader />}>
           <directionalLight position={[1, 1, 1]} intensity={2} />
           <ambientLight intensity={0.5} />
-          <pointLight position={[10, 5, 10]} intensity={2} />
-          <spotLight
-            position={[0, 50, 10]}
-            angle={0.15}
-            penumbra={1}
-            intensity={2}
-          />
           <hemisphereLight
             skyColor='#b1e1ff'
             groundColor='#000000'
             intensity={1}
           />
-          <Island />
+          <Bird/>
+          <Sky/>
+          <Island position={islandPosition} scale={islandScale} rotation={rotation} />
+          <Plane/>
         </Suspense>
 
 
